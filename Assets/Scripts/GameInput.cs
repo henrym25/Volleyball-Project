66228023b3a8cfb513af9290b2c9a7a967607ad7
henrym25 <henry3399@hotmail.co.nz>
2023-08-27 13:44:22 +1200

    Added X Bot with movement

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,26 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour {
-    public event EventHandler OnJumpAction;
-    private PlayerInput playerInput;
+
+    private PlayerInputActions playerInputActions;
 
     private void Awake() {
-        playerInput = new PlayerInput();
-        playerInput.Player.Enable();
-
-        playerInput.Player.Jump.performed += Jump_performed;
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
-    private void Jump_performed(InputAction.CallbackContext obj) {
-       OnJumpAction?.Invoke(this, EventArgs.Empty);
-    }
+    public Vector2 GetNormalizedMovmentVector() {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
 
-    public Vector2 GetNormalizedMovementVector() {
-        Vector2 inputVector = playerInput.Player.Move.ReadValue<Vector2>();
+        Debug.Log(inputVector);
 
         return inputVector.normalized;
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-    // Start is called before the first frame update
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private GameInput gameInput;
-    [SerializeField] private float JUMP_HEIGHT = 50f;
-    [SerializeField] private float MOVEMENT_SPEED = 7f;
-    [SerializeField] private Rigidbody playerBody;
 
-    private void Start() {
-        gameInput.OnJumpAction += GameInput_OnJumpAction; 
-    }
-
-    private void GameInput_OnJumpAction(object sender, System.EventArgs e) {
-        playerBody.AddForce(Vector3.up * JUMP_HEIGHT);
-    }
-
-    // Update is called once per frame
     private void Update() {
-        HandleMovement();
+        Vector2 inputVector = gameInput.GetNormalizedMovmentVector();
 
-    }
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-
-
-    private void HandleMovement() {
-        Vector2 inputVector = gameInput.GetNormalizedMovementVector();
-        Vector3 movementVector = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        transform.position += movementVector * Time.deltaTime * MOVEMENT_SPEED;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
